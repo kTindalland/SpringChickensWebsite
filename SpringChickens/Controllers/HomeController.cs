9941a2e691df3ea5bf5778cfac640073e0f616b1
Kai Tindall <kaitindall@outlook.com>
2019-10-10 11:54:59 +0100

    Added Kai Page

--- SpringChickens/Controllers/HomeController.cs
+++ SpringChickens/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace SpringChickens.Controllers
             return View();
         }
 
+        public IActionResult Kai()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
